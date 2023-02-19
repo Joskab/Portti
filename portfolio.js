eca65005e6f408329e78b5c947e802cc7a04a012
Joskab <83138339+Joskab@users.noreply.github.com>
2023-02-19 13:21:16 +0200

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -10,3 +10,30 @@ projectCards.forEach((card) => {
   const title = card.querySelector("h3");
   title.addEventListener("click", toggleDescription);
 });
+
+// Define an array of colors to transition between
+const colors = [
+  "#ffffff", // white
+  "#f5f5f5",
+  "#e0e0e0",
+  "#cccccc",
+  "#cccccc",
+  "#e0e0e0",
+  "#f5f5f5",
+  "#ffffff", // white
+];
+
+// Set the index of the current color
+let index = 0;
+
+// Function to change the background color
+function changeBackgroundColor() {
+  // Set the background color to the next color in the array
+  document.body.style.backgroundColor = colors[index];
+
+  // Increment the index, or reset it to 0 if it reaches the end of the array
+  index = (index + 1) % colors.length;
+}
+
+// Set an interval to change the background color every 2 seconds
+setInterval(changeBackgroundColor, 2000);
--- yhteydenotto.php
+++ yhteydenotto.php
@@ -1,16 +1,67 @@
 <?php
+// Check if the form has been submitted
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
     $to = 'joska.riittinen00@gmail.com';
     $subject = 'Uusi yhteydenotto!';
-    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $body = "Nimi: $name\nSähköposti: $email\nViesti: $message";
     $headers = "From: $email\r\nReply-To: $email\r\n";
     if (mail($to, $subject, $body, $headers)) {
-        echo "Kiitos viestistäsi!";
+        // Set a message to display to the user
+        $message = 'Viesti lähetettiin onnistuneesti!';
     } else {
-        echo "Viestiä lähettäessä tapahtui odottamaton virhe.";
+        $error = 'Viestiä lähettäessä tapahtui odottamaton virhe.';
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ota yhteyttä</title>
+    <link rel="stylesheet" href="yhteydenotto.css" />
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.html">Koti</a></li>
+          <li><a href="minusta.html">Minusta</a></li>
+          <li><a href="projektit.html">Projektini</a></li>
+          <li><a href="yhteydenotto.html">Ota yhteyttä</a></li>
+        </ul>
+      </nav>
+    </header>
+    <?php if (isset($message)) { ?>
+        <p><?php echo $message; ?></p>
+    <?php } ?>
+    <?php if (isset($error)) { ?>
+        <p><?php echo $error; ?></p>
+    <?php } ?>
+    <section class="contact">
+      <h2>Ota yhteyttä</h2>
+      <p>Mikäli sinulla on kysyttävää tai muita ideoita, ota yhteyttä!</p>
+      <form class="contact-form" action="yhteydenotto.php" method="POST">
+        <label for="name">Nimi</label>
+        <input type="text" id="name" name="name" required />
+
+        <label for="email">Sähköposti</label>
+        <input type="email" id="email" name="email" required />
+
+        <label for="message">Viesti</label>
+        <textarea id="message" name="message" rows="5" required></textarea>
+
+        <button type="submit" name="submit" class="btn-primary">
+          Lähetä viesti
+        </button>
+      </form>
+    </section>
+    <footer>
+      <p>Copyright &copy; 2023 Joska Riittinen All rights reserved.</p>
+    </footer>
+  </body>
+</html>
